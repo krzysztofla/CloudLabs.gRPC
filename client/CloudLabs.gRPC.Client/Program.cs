fe094aadba0a2caeb01b1e34484456bbbf27644c
krzysztofla <krzysztof.lach@icloud.com>
2022-02-09 11:31:40 +0100

    Example response request implementation for testing purpose

--- client/CloudLabs.gRPC.Client/Program.cs
+++ client/CloudLabs.gRPC.Client/Program.cs
@@ -1,3 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using CloudLabs.gRPC.API;
+using Grpc.Net.Client;
+
 Console.WriteLine("Hello, World!");
+
+using var channel = GrpcChannel.ForAddress("https://localhost:7179");
+var client = new Greeter.GreeterClient(channel);
+
+var response = await client.SayHelloAsync(new HelloRequest() {Name = "Kris"});
+
+Console.WriteLine(response);
